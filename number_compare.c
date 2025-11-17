cb0cf7b26f51720d9e9ec6de4d8735ccd1170412
Zach <104938351+zevlo@users.noreply.github.com>
2025-11-17 13:50:04 -0500

    Create number_compare.c

---
+++ number_compare.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int a;
+    int b;
+
+    printf("Enter first number: ");
+    scanf("%d", &a);
+
+    printf("Enter second number: ");
+    scanf("%d", &b);
+
+    if (a < b) {
+        printf("%d is less than %d\n", a, b);
+    }
+
+    if (a == b) {
+        printf("%d is equal to %d\n", a, b);
+    }
+
+    if (a > b) {
+        printf("%d is greater than %d\n", a, b);
+    }
+
+    return 0;
+}
